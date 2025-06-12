7c233a0dfa8e011303b339023a487b345ef3bf30
Yaroslav Lukovetskyi <111358303+Yaroslav0102030405@users.noreply.github.com>
2025-06-12 18:46:33 +0300

    lesson-23

--- lesson-22.js
+++ lesson-22.js
@@ -26,3 +26,46 @@ function fetchPokemonById(id) {
 fetchPokemonById(1)
   .then(pokemon => console.log(pokemon))
   .catch(error => console.log(error));
+
+//
+// import API from './api-service.js';
+
+const containerRef = document.querySelector('.js-con');
+const searchForm = document.querySelector('.js-form4');
+
+searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  fetchPokemon(searchQuery)
+    .then(pokemon => {
+      containerRef.innerHTML = renderPokemonCard(pokemon);
+    })
+    // .catch((error) => console.log(error))
+    .catch(onFetchError)
+    .finally(() => {
+      form.reset();
+    });
+}
+
+function fetchPokemon(pokemonId) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+  return fetch(url).then(response => {
+    return response.json();
+  });
+}
+
+function renderPokemonCard({ sprites, front_default, name, weight, height }) {
+  return `<img src="${sprites.front_default}" />
+  <h1>${name}</h1>
+  <p>${weight}</p>
+  <p>${height}</p>`;
+}
+
+function onFetchError() {
+  alert('Что-то пошло не так');
+}
